7547064a38c080e8e98a590f9842475e19109f99
KamiliArsyad <arsyad.ik@gmail.com>
2023-07-27 15:38:19 +0800

    Video input

---
+++ Dev/CloudMonocularVideo.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <chrono>
+
+#include <opencv2/core/core.hpp>
+
+#include <System.h>
+
+using namespace std;
+
+void LoadImages(const string &strImagePath, const string &strPathTimes,
+                vector<string> &vstrImages, vector<double> &vTimeStamps);
+
+int main(int argc, char **argv)
+{
+    if (argc < 5)
+    {
+        cerr << endl
+             << "Usage: ./mono_euroc path_to_vocabulary path_to_settings path_to_video_file frames_to_capture (trajectory_file_name)" << endl;
+        return 1;
+    }
+
+    bool bFileName = (((argc - 3) % 2) == 1);
+    string file_name;
+    if (bFileName)
+    {
+        file_name = string(argv[argc - 1]);
+        cout << "file name: " << file_name << endl;
+    }
+
+    int num_of_frames = atoi(argv[4]);
+
+    cout << endl
+         << "-------" << endl;
+    cout.precision(17);
+
+    int fps = 20;
+    float dT = 1.f / fps;
+    // Create SLAM system. It initializes all system threads and gets ready to process frames.
+    ORB_SLAM3::System SLAM(argv[1], argv[2], ORB_SLAM3::System::MONOCULAR, true);
+    float imageScale = SLAM.GetImageScale();
+
+    double t_resize = 0.f;
+    double t_track = 0.f;
+    // Main loop
+    cv::Mat im;
+    cv::VideoCapture input(argv[3]);
+    if (!input.isOpened())
+    {
+        cerr << endl
+             << "Failed to open video file at: "
+             << string(argv[3]) << endl;
+        return 1;
+    }
+
+    for (int i = 0; i < num_of_frames; i++)
+    {
+
+        // Read image from file and get time stamp
+        input >> im;
+        double tframe = input.get(cv::CAP_PROP_POS_MSEC) / 1000.0;
+
+        if (imageScale != 1.f)
+        {
+            int width = im.cols * imageScale;
+            int height = im.rows * imageScale;
+            cv::resize(im, im, cv::Size(width, height));
+        }
+
+
+        // Pass the image to the SLAM system
+        // cout << "tframe = " << tframe << endl;
+        SLAM.TrackMonocular(im, tframe);
+    }
+
+    // Stop all threads
+    SLAM.Shutdown();
+
+    // Save camera trajectory
+    if (bFileName)
+    {
+        const string kf_file = "kf_" + string(argv[argc - 1]) + ".txt";
+        const string f_file = "f_" + string(argv[argc - 1]) + ".txt";
+        SLAM.SaveTrajectoryEuRoC(f_file);
+        SLAM.SaveKeyFrameTrajectoryEuRoC(kf_file);
+    }
+    else
+    {
+        SLAM.SaveTrajectoryEuRoC("CameraTrajectory.txt");
+        SLAM.SaveKeyFrameTrajectoryEuRoC("KeyFrameTrajectory.txt");
+    }
+
+    return 0;
+}
+
+void LoadImages(const string &strImagePath, const string &strPathTimes,
+                vector<string> &vstrImages, vector<double> &vTimeStamps)
+{
+    ifstream fTimes;
+    fTimes.open(strPathTimes.c_str());
+    vTimeStamps.reserve(5000);
+    vstrImages.reserve(5000);
+    while (!fTimes.eof())
+    {
+        string s;
+        getline(fTimes, s);
+        if (!s.empty())
+        {
+            stringstream ss;
+            ss << s;
+            vstrImages.push_back(strImagePath + "/" + ss.str() + ".png");
+            double t;
+            ss >> t;
+            vTimeStamps.push_back(t * 1e-9);
+        }
+    }
+}
